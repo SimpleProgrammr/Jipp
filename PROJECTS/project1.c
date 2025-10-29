9479d856444b62448d85fb0114e40b4d89e3c113
Programmr <michal.pelon@proton.me>
2025-10-29 10:31:37 +0100

    Update Project 1/ tem.23

--- PROJECTS/project1.c
+++ PROJECTS/project1.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <tgmath.h>
 #include <limits.h>
+#include <locale.h>
 #include <stdlib.h>
+#include <wchar.h>
 
 void clearStdin();
 
 
-enum STOP_REASON { MAX_M, MAX_DEF_PRECISION, NORMAL };
+enum STOP_REASON { MAX_M, MAX_DEF_PRECISION, ABNORMAL };
 
 long long int safe_convert(long double ld) {
     // Check if value is within range
@@ -29,7 +31,7 @@ long long int safe_convert(long double ld) {
 long double f(long double x, long double delta_y, int M, int *stop_res, int *used_M) {
     long double y = x;
     long double element;
-    int u_M = 1;
+    int u_M = 0;
     int iteration = 2;
     do {
         element = pow(-1, iteration-1) * (pow(x, iteration) / iteration);
@@ -51,38 +53,42 @@ int main() {
     //Wczytaj przedział
     //wczytaj ilość podziałów
     //Spradz czy delta nie jest < 1e-14
+
+    setlocale(LC_ALL, "pl_PL.CP1250");
+
     long double a = 0, b = 0, nc = 0, deltaY = 0;
     long long int n = 0;
-    printf("Interpretacja zakresu:\n----A------B---->x\n");
-    printf("A <= x <= B\nZakres musi znajdowac sie w przedziale -1 < x <= 1\n");
+    printf("Liczymy Ln(1+x) !!!");
+    printf("Interpretacja zakresu:\n---(A------B>--->x\n");
+    printf("A < x <= B\nZakres musi znajdować się w przedziale -1 < x <= 1\n");
     printf("Podaj początek zakresu (A): ");
 
     if (scanf("%Le", &a) != 1 || a < -1 || a > 1 || isnan(a)) {
-        fprintf(stderr, "Bledne dane wejsciowe\n");
+        fprintf(stderr, "Błędne dane wejściowe\n");
         return 400;
     }
     clearStdin();
     printf("Podaj koniec zakresu (B): ");
     if (scanf("%Le", &b) != 1 || b < -1 || b > 1 || b <= a || isnan(b)) {
-        fprintf(stderr, "Bledne dane wejsciowe\n");
+        fprintf(stderr, "Błędne dane wejściowe\n");
         return 400;
     }
     clearStdin();
-    printf("Podaj ilość podziałów (N > 0): ");
+    printf("Podaj ilość podziałów (0 < N < %d): ",INT_MAX);
     if (scanf("%Le", &nc) != 1 || nc <= 0 || round(nc) != nc) {
-        fprintf(stderr, "Bledne dane wejsciowe\n");
+        fprintf(stderr, "Błędne dane wejściowe\n");
         return 400;
     }
     clearStdin();
-    printf("Podaj dokładność wyników (DeltaY > 0): ");
-    if (scanf("%Le", &deltaY) != 1 || deltaY <= 0 || isnan(deltaY) || isinf(deltaY)) {
-        fprintf(stderr, "Bledne dane wejsciowe\n");
+    printf("Podaj dokładność wyników (0 < DeltaY < 1e-16): ");
+    if (scanf("%Le", &deltaY) != 1 || deltaY < 1e-16 || isnan(deltaY) || isinf(deltaY)) {
+        fprintf(stderr, "Błędne dane wejściowe\n");
         return 400;
     }
     clearStdin();
     n = safe_convert(nc);
     if (n < 0) {
-        printf("Bledne dane wejsciowe\n");
+        printf("Błędne dane wejściowe\n");
     }
     long double deltaX = (b - a) / (long double) n;
     if (deltaX < LDBL_EPSILON) {
@@ -92,7 +98,7 @@ int main() {
     int M = 1000000000;
     printf("Podaj maksymalną ilość iteracji (0 < M < %d): ", INT_MAX);
     if (scanf("%d", &M) != 1 || M <= 0 || isnan(M) || isinf(M) || M > INT_MAX) {
-        fprintf(stderr, "Bledne dane wejsciowe\n");
+        fprintf(stderr, "Błędne dane wejściowe\n");
         return 400;
     }
     clearStdin();
@@ -102,33 +108,44 @@ int main() {
     file = fopen("Project_1.txt", "w");
 
     if (file == NULL) {
-        fprintf(stderr, "Blad pliku!\n");
+        fprintf(stderr, "Błąd pliku!\n");
         exit(404);
     }
 
-    printf("Lp.\t\tX\t\t\t\tf_szereg(X)\t\tf_scisle(X)\t\tWyrazy_szeregu\t\tPowod zatrzymania\n");
-    fprintf(file, "Lp.\t\tX\t\t\t\tf_szereg(X)\t\tf_scisle(X)\t\tWyrazy_szeregu\t\tPowod zatrzymania\n");
+    printf("Lp.\t\tX\t\t\t\tf_szereg(X)\t\t\t\tf_ściśle(X)\t\t\t\tWyrazy_szeregu\t\t\t\tPowód zatrzymania\n");
+    fprintf(file, "Lp.\t\tX\t\t\t\tf_szereg(X)\t\t\t\tf_ściśle(X)\t\t\t\tWyrazy_szeregu\t\t\t\tPowód zatrzymania\n");
     long double x = a + deltaX;
     for (long long counter = 1; counter <= n; counter++) {
-        int stop_res = NORMAL;
+        int stop_res = ABNORMAL;
         int used_M = 0;
         long double f_x = f(x, deltaY, M, &stop_res, &used_M);
-
+        //stop_res = ABNORMAL;
         char *stop_msg;
         switch (stop_res) {
             case MAX_M:
-                stop_msg = "Przekroczono ilosc iteracji";
+                stop_msg = "Osiągnięto maksymalną ilość iteracji";
                 break;
             case MAX_DEF_PRECISION:
-                stop_msg = "Osiegnieto zadana dokladnosc";
+                stop_msg = "Osiągnieto żadaną dokładność";
                 break;
             default:
-                stop_msg = "Praca normalna";
-                break;
+                stop_msg = "Praca nie spodziewana";
+                fprintf(stderr, "%s\n"
+                        "Użyte M = %d\n"
+                        "M = %d\n"
+                        "X = %.10Lg\n"
+                        "F(x) = %.10Lg\t->\tln(1+x) = %.10Lg\n"
+                        "N = %lld\n"
+                        "Delta X = %.10Lg\n"
+                        "Delta Y = %.10Lg\n\n", stop_msg, used_M, M, x, f_x, log(1+x), n, deltaX, deltaY);
+                exit(123);
         }
 
-        printf("%lld\t\t%Le\t%Le\t%le\t%le\t\t%s\n", counter, x, f_x, log(1+x), (double) used_M, stop_msg);
-        fprintf(file, "%lld\t\t%Le\t%Le\t%le\t%le\t\t%s\n", counter, x, f_x, log(1+x), (double) used_M, stop_msg);
+        printf("%-5lld %-15.6Le %-24.16Lg %-24.16Lg %-10d \t\t\t\t%s\n", counter, x, f_x, logl(1 + x), used_M,
+               stop_msg);
+        fprintf(file, "%-5lld %-15.6Le %-24.16Lg %-24.16Lg %-10d \t\t\t\t%s\n", counter, x, f_x, logl(1 + x), used_M,
+                stop_msg);
+
         x += deltaX;
     }
 
