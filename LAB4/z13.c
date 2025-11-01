eb2ce5fb8525d50cfab0bbc2902cc7f7e7100576
Programmr <michal.pelon@proton.me>
2025-11-01 14:52:43 +0100

    LAB4 Ready + .gitignore

--- LAB4/z13.c
+++ LAB4/z13.c
@@ -3,16 +3,15 @@
 
 int main() {
     FILE *f = NULL;
-    f = fopen("z10.txt", "w+");
+    f = fopen("z13.txt", "w+");
     if (f == NULL) {
         printf("File could not be opened\n");
         return 404;
     }
-
     fprintf(f, "I <3 programming\n");
     fclose(f);
 
-    f = fopen("z10.txt", "r");
+    f = fopen("z13.txt", "r");
     char c;
     while (fscanf(f, "%c", &c) != EOF) {
         printf("%c\n", c);
--- LAB4/z5.c
+++ LAB4/z5.c
@@ -1,10 +1,20 @@
+#include <locale.h>
 #include <stdio.h>
+#include <windows.h>
 
 int main() {
+    setlocale(LC_ALL, "pl_PL.UTF-8");
+    setlocale(LC_NUMERIC, "C");
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
     int n = 0;
     int silnia = 1;
-    printf("Podaj liczbe: ");
-    if (scanf("%d", &n) != 1) return 10;
+    printf("Podaj liczbe całkowitą (części dziesiętnę oraz znaki po ',' lub '.' będą ignorowane): ");
+    if (scanf("%d", &n) != 1) {
+        fprintf(stderr, "Błędne dane wejściowe.\n");
+        return 10;
+    };
 
     for (int i = 1; i <= n; i++) {
         silnia = silnia * i;
