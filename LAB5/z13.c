c8db8f9aec7232084570fbf6114c72377c06eb22
Programmr <michal.pelon@proton.me>
2025-11-07 11:41:20 +0100

    LAB5 Finished README.md Updated Projekt1_tematy.pdf moved to PROJECTS/TOPICS

--- LAB5/z13.c
+++ LAB5/z13.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int replace(char *, char);
+int rotare(char *, char);
 
 int main() {
     char tekst[256];
@@ -12,10 +12,10 @@ int main() {
     printf("Please enter char: ");
     znak = getchar();
 
-    replace(tekst, znak);
+    rotare(tekst, znak);
 }
 
-int replace(char *txt, char c) {
+int rotare(char *txt, char c) {
     for (int i = 0; txt[i] != '\n'; i++) {
         if (txt[i] == c)
             printf("In text: %s, char '%c' was found on %d index\n", txt, c, i);
