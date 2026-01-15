71d429eca6b0caca8aaa144f9a8f03186de5945b
Programmr <michal.pelon@proton.me>
2026-01-15 14:30:50 +0100

    Project 4 - menu fix

--- PROJECTS/Project 4/project4.c
+++ PROJECTS/Project 4/project4.c
@@ -1,4 +1,3 @@
-#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -7,6 +6,41 @@
 #include "FIFO-array.c"
 #include "LIFO-list.c"
 #include "LIFO-array.c"
+#ifdef _WIN32
+    #include <conio.h>
+#else
+    #include <termios.h>
+    #include <unistd.h>
+
+    char getch() {
+        char buf = 0;
+        struct termios old = {0};
+        fflush(stdout);
+
+        if (tcgetattr(0, &old) < 0)
+            perror("tcsetattr()");
+
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+
+        if (tcsetattr(0, TCSANOW, &old) < 0)
+            perror("tcsetattr ICANON");
+
+        if (read(0, &buf, 1) < 0)
+            perror("read()");
+
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+
+        if (tcsetattr(0, TCSADRAIN, &old) < 0)
+            perror("tcsetattr ~ICANON");
+
+        return buf;
+    }
+#endif
+
 
 enum DATA_TYPE_MODE {
     FIFO_LIST = 1,
